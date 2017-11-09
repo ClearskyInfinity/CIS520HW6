4852c5b8a0b6f4478fb8276c5f2cb531182e7207
Yifan Li <yifan_frank_li@outlook.com>
2017-11-09 17:50:28 -0500

    Added 5.1

--- my_test.m
+++ my_test.m
@@ -1,9 +1,6 @@
-a = [1, 2];
-b = [3, 4];
-c = [5, 6];
-
-buckets = {a, b, c};
-
-t = cat(1, buckets{:});
-
-disp(sum(t, 1));
+% a = [1, 2];
+% b = [3, 4];
+% c = [5, 6];
+% buckets = {a, b, c};
+% t = cat(1, buckets{:});
+% disp(sum(t, 1));
